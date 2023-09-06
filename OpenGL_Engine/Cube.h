b84949179054ef907010c4c1f588a63b1c59d4e4
Nala Mthembu <Nala00.12@gmail.com>
2023-09-06 13:53:18 +0200

    Who?

---
+++ OpenGL_Engine/Cube.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "GameObject.h"
+
+class Cube : public GameObject
+{
+private:
+	void drawGeometry() override;
+};
---
+++ OpenGL_Engine/TextureManager.cpp
@@ -0,0 +1,31 @@
+#include "TextureManager.h"
+#include "Texture.h"
+#include <iostream>
+
+TextureManager::TextureManager()
+{
+	textures.insert({ "brick", new Texture("Textures/bricks.png") });
+}
+
+TextureManager::~TextureManager()
+{
+	map<string, Texture*>::iterator it;
+
+	for (it = textures.begin(); it != textures.end(); it++)
+	{
+		delete it->second;
+	}
+}
+
+
+void TextureManager::useTexture(string textureName)
+{
+	if (!textures.count(textureName))
+	{
+		std::cout << "WARNING : texture does not exist!" << std::endl;
+		return;
+	}
+
+	Texture* tex = textures[textureName];
+	tex->use();
+}
